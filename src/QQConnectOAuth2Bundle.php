c5096f0effa48a4159c7c3aac0be7c362044b571
github-ci <you@example.com>
2025-11-18 22:21:54 +0800

    fix test

--- src/QQConnectOAuth2Bundle.php
+++ src/QQConnectOAuth2Bundle.php
@@ -6,6 +6,7 @@ use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
 use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 use Tourze\RoutingAutoLoaderBundle\RoutingAutoLoaderBundle;
 
 class QQConnectOAuth2Bundle extends Bundle implements BundleDependencyInterface
@@ -16,6 +17,7 @@ class QQConnectOAuth2Bundle extends Bundle implements BundleDependencyInterface
             DoctrineBundle::class => ['all' => true],
             DoctrineTimestampBundle::class => ['all' => true],
             RoutingAutoLoaderBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
